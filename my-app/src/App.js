1ca9d0ce2450609e24dac0bb7ac3322b2f16d288
alekspanteli <alekspanteli@rambler.ru>
2022-08-16 17:06:33 +0300

    add components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
 import Navbar from './Navbar';
 
 const App = () => {
   return (
-    <div>
-      <React.Fragment>
+    <>
         <Navbar title="Lots of components" />
         <div class="bg-dark">Hello World</div>
-      </React.Fragment>
-    </div>
+    </>
   );
 };
 
