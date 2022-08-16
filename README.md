bcf1ab54efc2f0ddc345601dc8c002cacfccb79b
alekspanteli <alekspanteli@rambler.ru>
2022-08-16 16:47:29 +0300

    add bootstrap

--- README.md
+++
@@ -1,3 +0,0 @@
-# node-hhz2ys
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-hhz2ys)
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 
