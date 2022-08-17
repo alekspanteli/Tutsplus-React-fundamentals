f108f7ce2ee7c718ab07809832dd13d74f79428c
alekspanteli <alekspanteli@rambler.ru>
2022-08-17 17:23:26 +0300

    add react state buttons

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,24 +1,36 @@
 import { useState } from 'react';
 
 const Counter = (props) => {
-
   const [counter, setCounter] = useState(props.startAt);
 
-  setTimeout(() => setCounter(counter + props.countBy), 1000);
+  // setTimeout(() => setCounter(counter + props.countBy), 1000);
+
+  const CountUp = (e) => {
+    setCounter(counter + props.countBy);
+    console.log(e);
+  }
+
+  const CountDown = (e) => {
+    setCounter(counter - props.countBy);
+    console.log(e);
+  }
 
   return (
     <>
       <p>Start at: {props.startAt}</p>
       <p>Count By: {props.countBy}</p>
       <h4>{counter}</h4>
+      <div className="d-flex gap-1">
+        <button className="btn btn-primary" onClick={CountUp}>Count Up</button>
+        <button className="btn btn-danger" onClick={CountDown}>Count Down</button>
+      </div>
     </>
-  )
-}
+  );
+};
 
 Counter.defaultProps = {
   startAt: 0,
-  countBy: 1
-}
+  countBy: 1,
+};
 
 export default Counter;
-
